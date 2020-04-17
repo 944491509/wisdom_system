7e2eece3ba5bb0a135b40773cc5ec7b5c72584f3
liuyang <944491509@qq.com>
2020-04-17 14:59:40 +0800

    添加课程表

--- app/Http/Controllers/Api/School/TimeSlotsController.php
+++ app/Http/Controllers/Api/School/TimeSlotsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\School;
 
+use App\Dao\Schools\GradeDao;
 use App\Dao\Schools\SchoolDao;
 use App\Dao\Timetable\TimeSlotDao;
 use App\Http\Requests\MyStandardRequest;
@@ -53,13 +54,13 @@ class TimeSlotsController extends Controller
 
     /**
      * 加载所有的学习时间段, 以及学期中用来学习的总周数
-     * @param Request $request
+     * @param MyStandardRequest $request
      * @return string
      */
-    public function load_study_time_slots(Request $request){
+    public function load_study_time_slots(MyStandardRequest $request){
         $schoolIdOrUuid = $request->get('school');
         $noTime = $request->get('no_time', false);
-
+        $gradeId = $request->getGradeId();
         $school = null;
         $schoolDao = new SchoolDao(new User());
 
@@ -76,12 +77,16 @@ class TimeSlotsController extends Controller
         if($schoolIdOrUuid && $school){
             $timeSlotDao = new TimeSlotDao();
             $field = ConfigurationTool::KEY_STUDY_WEEKS_PER_TERM;
-            return JsonBuilder::Success(
-                [
-                    'time_frame'=>$timeSlotDao->getAllStudyTimeSlots($schoolIdOrUuid, true, $noTime),
-                    'total_weeks'=>$school->configuration->$field,
-                ]
-            );
+            $gradeDao = new GradeDao();
+            $grade = $gradeDao->getGradeById($gradeId);
+
+            $timeFrame = $timeSlotDao->getAllStudyTimeSlots($schoolIdOrUuid, $grade->gradeYear(),true, $noTime);
+            $data = [
+                'time_frame'=> $timeFrame,
+                'total_weeks'=>$school->configuration->$field,
+            ];
+
+            return JsonBuilder::Success($data);
         }
         return JsonBuilder::Error();
     }
--- resources/js/components/previewer/TimetableItemForm.vue
+++ resources/js/components/previewer/TimetableItemForm.vue
@@ -82,9 +82,9 @@
                                 </el-select>
                                 <span class="help-text">
                                 说明: 请选择要教授哪门课程
-                                <span v-if="timeTableItem.course_id !== ''">
+                                <!--<span v-if="timeTableItem.course_id !== ''">
                                     <a target="_blank" :href="previewByCourseUrl">点击查看({{ courseText }})的已有课程安排</a>
-                                </span>
+                                </span>-->
                             </span>
                             </el-form-item>
 
@@ -94,9 +94,9 @@
                                 </el-select>
                                 <span class="help-text">
                                     说明: 请选择授课的老师
-                                    <span v-if="timeTableItem.teacher_id !== ''">
+                                    <!--<span v-if="timeTableItem.teacher_id !== ''">
                                         <a target="_blank" :href="previewByTeacherUrl">点击查看老师: ({{ teacherText }}) 已被安排的课程</a>
-                                    </span>
+                                    </span>-->
                                 </span>
                             </el-form-item>
                             <el-form-item label="教学楼">
@@ -119,11 +119,12 @@
                                 <el-select v-model="timeTableItem.room_id" style="width: 100%;">
                                     <el-option :label="room.name + ': ' + room.description + '(可容纳: ' + room.seats +'人)'" :value="room.id" :key="room.id" v-for="room in rooms"></el-option>
                                 </el-select>
-                                <span v-if="timeTableItem.room_id !== ''">
-                                    <a target="_blank" :href="previewByRoomUrl">点击查看教室: ({{ locationText }}) 的排课</a>
-                                </span>
-                                <span v-else class="help-text">
+
+                                <span class="help-text">
                                     说明: 请选择上面选择的楼的那个房间上课
+                                    <!--<span v-if="timeTableItem.room_id !== ''">-->
+                                    <!--<a target="_blank" :href="previewByRoomUrl">点击查看教室: ({{ locationText }}) 的排课</a>-->
+                                <!--</span>-->
                                 </span>
                             </el-form-item>
                         </div>
