ebf33476943da217e435b38feb3d73091427669c
Cherish <791832287@qq.com>
2020-04-23 13:36:54 +0800

    update

--- resources/js/includes/backend/teacher-attendance_manager/components/Organization.js
+++ resources/js/includes/backend/teacher-attendance_manager/components/Organization.js
@@ -4,7 +4,7 @@ import { _load_children, catchErr } from "../api/index";
 Vue.component("Organization", {
   template: `
   <el-form-item label="考勤部门" prop="organizations">
-    <el-cascader v-if="isCreated" style="width: 100%;" :props="props" :options="organizations"  v-model="formData.organizations"></el-cascader> 
+    <el-cascader v-if="isCreated" style="width: 100%;" :props="props" :options="organizations"  v-model="formData.organizations"></el-cascader>
     <el-cascader  v-else style="width:100%;" :props="propsOptions" :options= "organizations"  v-model="formData.organizations"></el-cascader>
   </el-form-item>
   `,
@@ -30,6 +30,7 @@ Vue.component("Organization", {
       props: {
         lazy: true,
         multiple: true,
+        checkStrictly:true,
         value: "id",
         label: "name",
         lazyLoad: (node, resolve) => this._lazyLoad(node, resolve)
@@ -37,6 +38,7 @@ Vue.component("Organization", {
       propsOptions: {
         lazy: false,
         multiple: true,
+        checkStrictly:true,
         value: "id",
         label: "name",
       },
