3cc56184f6b9d930de7e9faacf215f77eb919115
liuyang <944491509@qq.com>
2020-04-27 09:26:10 +0800

    教师端菜单导航

--- resources/views/layouts/desktop/teacher/sidebar.blade.php
+++ resources/views/layouts/desktop/teacher/sidebar.blade.php
@@ -58,7 +58,7 @@ $teacher = \Illuminate\Support\Facades\Auth::user();
                 <li class="nav-item">
                     <a href="{{ route('teacher.ly.assistant.index') }}" class="nav-link">
                         <i class="material-icons">local_library</i>
-                        <span class="title">助手</span>
+                        <span class="title">教学/教务</span>
                         <span class="arrow nav-toggle"></span>
                     </a>
                     <ul class="sub-menu">
@@ -74,88 +74,88 @@ $teacher = \Illuminate\Support\Facades\Auth::user();
                     </ul>
                 </li>
 
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="material-icons">map</i>
-                        <span class="title">教学管理</span>
-                        <span class="arrow nav-toggle"></span>
-                    </a>
-                    <ul class="sub-menu">
-                        @foreach($teacher->myCourses as $uc)
-                        @include('layouts.desktop.elements.submenu_selected',['routeName'=>'teacher.course.materials.manager','routeParams'=>['teacher'=>$teacher->id,'course_id'=>$uc->course->id],'name'=>$uc->course->name])
-                        @endforeach
-                    </ul>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a href="#" class="nav-link">--}}
+{{--                        <i class="material-icons">map</i>--}}
+{{--                        <span class="title">教学管理</span>--}}
+{{--                        <span class="arrow nav-toggle"></span>--}}
+{{--                    </a>--}}
+{{--                    <ul class="sub-menu">--}}
+{{--                        @foreach($teacher->myCourses as $uc)--}}
+{{--                        @include('layouts.desktop.elements.submenu_selected',['routeName'=>'teacher.course.materials.manager','routeParams'=>['teacher'=>$teacher->id,'course_id'=>$uc->course->id],'name'=>$uc->course->name])--}}
+{{--                        @endforeach--}}
+{{--                    </ul>--}}
+{{--                </li>--}}
 
-                <li class="nav-item">
-                    <a href="javascript:void(0);" class="nav-link nav-toggle">
-                        <i class="material-icons">store</i>
-                        <span class="title">考试管理</span>
-                        <span class="selected"></span>
-                        <span class="arrow"></span>
-                    </a>
-                    <ul class="sub-menu">
-                        <li class="nav-item active">
-                            <a href="{{ route('teacher.exam.index') }}" class="nav-link ">
-                                <span class="title">考试列表</span>
-                                <span class="selected"></span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a href="javascript:void(0);" class="nav-link nav-toggle">--}}
+{{--                        <i class="material-icons">store</i>--}}
+{{--                        <span class="title">考试管理</span>--}}
+{{--                        <span class="selected"></span>--}}
+{{--                        <span class="arrow"></span>--}}
+{{--                    </a>--}}
+{{--                    <ul class="sub-menu">--}}
+{{--                        <li class="nav-item active">--}}
+{{--                            <a href="{{ route('teacher.exam.index') }}" class="nav-link ">--}}
+{{--                                <span class="title">考试列表</span>--}}
+{{--                                <span class="selected"></span>--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+{{--                    </ul>--}}
+{{--                </li>--}}
 
-                @if(session('school.id'))
-                    <li class="nav-item">
-                        {{--<a href="javascript:void(0);" class="nav-link nav-toggle">
-                            <i class="material-icons">event_seat</i>
-                            <span class="title">会议管理</span>
-                            <span class="selected"></span>
-                            <span class="arrow open"></span>
-                        </a>
-                        <ul class="sub-menu">
-                            <li class="nav-item active">
-                                <a href="" class="nav-link ">
-                                    <span class="title">分组列表</span>
-                                    <span class="selected"></span>
-                                </a>
-                            </li>
-                        </ul>--}}
-                        {{--<a href="{{ route('teacher.conference.index',['uuid'=>session('school.uuid')])  }}" class="nav-link">
-                            <i class="material-icons">event_seat</i>
-                            <span class="title">会议管理</span>
-                        </a>--}}
-                    </li>
+{{--                @if(session('school.id'))--}}
+{{--                    <li class="nav-item">--}}
+{{--                        --}}{{--<a href="javascript:void(0);" class="nav-link nav-toggle">--}}
+{{--                            <i class="material-icons">event_seat</i>--}}
+{{--                            <span class="title">会议管理</span>--}}
+{{--                            <span class="selected"></span>--}}
+{{--                            <span class="arrow open"></span>--}}
+{{--                        </a>--}}
+{{--                        <ul class="sub-menu">--}}
+{{--                            <li class="nav-item active">--}}
+{{--                                <a href="" class="nav-link ">--}}
+{{--                                    <span class="title">分组列表</span>--}}
+{{--                                    <span class="selected"></span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
+{{--                        --}}{{--<a href="{{ route('teacher.conference.index',['uuid'=>session('school.uuid')])  }}" class="nav-link">--}}
+{{--                            <i class="material-icons">event_seat</i>--}}
+{{--                            <span class="title">会议管理</span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
 
-                    <li class="nav-item">
-                        <a href="javascript:void(0);" class="nav-link nav-toggle">
-                            <i class="material-icons">people</i>
-                            <span class="title">招生管理</span>
-                            <span class="selected"></span>
-                            <span class="arrow open"></span>
-                        </a>
-                        <ul class="sub-menu">
-                            <li class="nav-item">
-                                <a href="{{ route('teacher.planRecruit.list') }}" class="nav-link ">
-                                    <span class="title">预招管理</span>
-                                    <span class="selected"></span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('school_manager.consult.list') }}" class="nav-link ">
-                                    <span class="title">咨询管理</span>
-                                    <span class="selected"></span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
+{{--                    <li class="nav-item">--}}
+{{--                        <a href="javascript:void(0);" class="nav-link nav-toggle">--}}
+{{--                            <i class="material-icons">people</i>--}}
+{{--                            <span class="title">招生管理</span>--}}
+{{--                            <span class="selected"></span>--}}
+{{--                            <span class="arrow open"></span>--}}
+{{--                        </a>--}}
+{{--                        <ul class="sub-menu">--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a href="{{ route('teacher.planRecruit.list') }}" class="nav-link ">--}}
+{{--                                    <span class="title">预招管理</span>--}}
+{{--                                    <span class="selected"></span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a href="{{ route('school_manager.consult.list') }}" class="nav-link ">--}}
+{{--                                    <span class="title">咨询管理</span>--}}
+{{--                                    <span class="selected"></span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
+{{--                    </li>--}}
 
-                    <li class="nav-item">
-                        <a href="{{ route('school_manager.facility.list') }}" class="nav-link">
-                            <i class="material-icons">dashboard</i>
-                            <span class="title">设备管理</span>
-                        </a>
-                    </li>
-                @endif
+{{--                    <li class="nav-item">--}}
+{{--                        <a href="{{ route('school_manager.facility.list') }}" class="nav-link">--}}
+{{--                            <i class="material-icons">dashboard</i>--}}
+{{--                            <span class="title">设备管理</span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                @endif--}}
             </ul>
         </div>
     </div>
