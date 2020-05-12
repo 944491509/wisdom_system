08e0b9d08581acf2399706f699032512ad82db90
zhaochunzheng <tracy0953@qq.com>
2020-05-12 21:53:31 +0800

    fix

--- resources/views/teacher/ly/assistant/grades_check_in.blade.php
+++ resources/views/teacher/ly/assistant/grades_check_in.blade.php
@@ -18,9 +18,9 @@
                             <el-select size="small" v-model="gradeValue" placeholder="请选择班级">
                                 <el-option
                                         v-for="item in gradeOptions"
-                                        :key="item.value"
-                                        :label="item.label"
-                                        :value="item.value">
+                                        :key="item.id"
+                                        :label="item.name"
+                                        :value="item.id">
                                 </el-option>
                             </el-select>
                             <el-button type="primary" size="small" @click="searchList()">查询</el-button>
