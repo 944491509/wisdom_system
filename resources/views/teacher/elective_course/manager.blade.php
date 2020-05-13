000e794f548ff75d63b5d6b51b9930345d1b8911
21dexiaoyu <21dexiaoyu@163.com>
2020-05-13 15:46:24 +0800

    选修课页面

---
+++ resources/views/teacher/elective_course/manager.blade.php
@@ -0,0 +1,9 @@
+<?php
+use App\Utils\UI\Anchor;
+use App\Utils\UI\Button;
+use App\User;
+?>
+@extends('layouts.app')
+@section('content')
+resources/views/teacher/elective_course/manager.blade.php
+@endsection
