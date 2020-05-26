d82ad7d570ef99aa7c2d07c1f56b110ec96b27d4
liuyang <944491509@qq.com>
2020-05-26 09:30:53 +0800

    其他班互换课

--- app/Models/Timetable/TimetableItem.php
+++ app/Models/Timetable/TimetableItem.php
@@ -35,7 +35,7 @@ class TimetableItem extends Model
     const TYPE_SUBSTITUTION_NOTHING = 3;  // 调课 他班互换
     const TYPE_SUPPLY_TEXT = '教师代课';
     const TYPE_SUBSTITUTION_TEXT = '本班课程课节互换';
-    const TYPE_SUBSTITUTION_NOTHING_TEXT = '本班课程课节互换';
+    const TYPE_SUBSTITUTION_NOTHING_TEXT = '其他班互换课';
 
     const PASSIVITY = 0; // 被动调课
     const INITIATIVE = 1; // 主动调课
