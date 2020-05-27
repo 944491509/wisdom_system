890f7bf6f18325cccc722ac70ab7cb52347fe38e
liuyang <944491509@qq.com>
2020-05-27 12:15:23 +0800

    云班牌添加缓存

--- app/Http/Controllers/Api/Cloud/CloudController.php
+++ app/Http/Controllers/Api/Cloud/CloudController.php
@@ -94,7 +94,7 @@ class CloudController extends Controller
         if (empty($facility)) {
             return JsonBuilder::Error('设备码错误,或设备已关闭');
         }
-        $res = Redis::get('grade:'.'code'.$code);
+        $res = Redis::get('grade:'.'code:'.$code);
         if(is_null($res)) {
             $room = $facility->room;
             $timeSlotDao = new TimeSlotDao;
@@ -149,7 +149,7 @@ class CloudController extends Controller
                 'photo' => $photo
             ];
             // 默认 60s
-            Redis::setex('grade:'.'code'.$code, 60 * 10, json_encode($data));
+            Redis::setex('grade:'.'code:'.$code, 60 * 10, json_encode($data));
         }
         else {
             $data = json_decode($res, true);
