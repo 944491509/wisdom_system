3cb2b8d03afb11a2f56d629bdaae64d266ad5e36
Cherish <791832287@qq.com>
2020-06-03 19:47:23 +0800

    update

--- resources/js/includes/backend/teacher_oa_notices_app.js
+++ resources/js/includes/backend/teacher_oa_notices_app.js
@@ -4,186 +4,172 @@
 import { Util } from "../../common/utils";
 import { Constants } from "../../common/constants";
 import { startedByMe, waitingForMe } from "../../common/flow";
-import Axios from "axios";
 
-if (document.getElementById('teacher-oa-notices-app')) {
-    new Vue({
-        el: '#teacher-oa-notices-app',
-        data() {
-            return {
-                schoolId: null,
-                userUuid: null,
-                url: {
-                    flowOpen: ''
-                },
-                isLoading: false,
-                flowsStartedByMe: [],
-                flowsWaitingForMe: [],
-                oneList: [],
-                twoList: [],
-                threeList: [],
-                detail: {},
-                drawer: false, // 通知侧边栏
-                titleName: "",
-                attachments: [], // 附件
-                releaseDrawer: false, // 发布通知drawer
-                form: {
-                  title: '',
-                  textarea: '',
-                  organizations: []
-                },
-                innerDrawer: false,
-                selecttags: [
-                  { name: '标签一', type: '' },
-                  { name: '标签二', type: 'success' },
-                  { name: '标签三', type: 'info' },
-                  { name: '标签四', type: 'warning' },
-                  { name: '标签五', type: 'danger' }
-                ],
-                props: {
-                  lazy: true,
-                  value: 'id',
-                  checkStrictly: true,
-                  label: 'name',
-                  expandTrigger:'hover',
-                  lazyLoad(node, resolve) {
-                      let parentId = null;
-                      if (!Util.isEmpty(node.data)) {
-                          parentId = node.data.id;
-                      }
-                      axios.post(
-                          Constants.API.ORGANIZATION.LOAD_CHILDREN,
-                          { level: node.level + 1, parent_id: parentId }
-                      ).then(res => {
-                          if (Util.isAjaxResOk(res)) {
-                              resolve(res.data.data.orgs);
-                          }
-                      });
-                  }
-                },
-                organizansList: ''
+if (document.getElementById("teacher-oa-notices-app")) {
+  new Vue({
+    el: "#teacher-oa-notices-app",
+    data() {
+      return {
+        schoolId: null,
+        userUuid: null,
+        url: {
+          flowOpen: ""
+        },
+        isLoading: false,
+        flowsStartedByMe: [],
+        flowsWaitingForMe: [],
+        oneList: [],
+        twoList: [],
+        threeList: [],
+        detail: {},
+        drawer: false, // 通知侧边栏
+        titleName: "",
+        attachments: [], // 附件
+        releaseDrawer: false, // 发布通知drawer
+        form: {
+          title: "",
+          textarea: "",
+          organizations: []
+        },
+        innerDrawer: false,
+        selecttags: [ ],
+        props: {
+          isLeaf: "status",
+          label: "name",
+          disabled:function(data,node){
+            if(data.status){
+              return false
             }
+            return true
+          }
         },
-        created() {
-            const dom = document.getElementById('app-init-data-holder');
-            this.schoolId = dom.dataset.school;
-            this.userUuid = dom.dataset.useruuid;
-            this.url.flowOpen = dom.dataset.flowopen;
-            this.getnoticeList1();
-            this.getnoticeList2();
-            this.getnoticeList3();
-            this.getOrganizansList()
-        },
-        methods: {
-          handleClose1() {
-            this.releaseDrawer = false
-          },
-          handleClose2() {
-            this.innerDrawer = false
-          },
-          deleteTag(tag) {
-            this.selecttags.splice(this.selecttags.indexOf(tag), 1);
-          },
-          reload() {
-
-          },
-          getOrganizansList(){
-            console.log('接口',Constants.API.ORGANIZATION.LOAD_ALL)
-            axios.post(
-                Constants.API.ORGANIZATION.LOAD_ALL,
-                { school_id: this.schoolId }
-            ).then(res => {
-              console.log('返回')
-                if (Util.isAjaxResOk(res)) {
-                  console.log('结果', res)
-                    this.organizansList = res.data.data;
-                }
-            });
-          },
-          // 最后发布接口
-          release() {
-
-          },
-            // 获取本页列表
-            getnoticeList1() {
-                axios.post(
-                    '/api/notice/notice-list',
-                    { type: 1 }
-                ).then(res => {
-                    if (Util.isAjaxResOk(res)) {
-                        this.oneList = res.data.data.list
-                    }
-                })
-            },
-            // 获取本页列表
-            getnoticeList2() {
-                axios.post(
-                    '/api/notice/notice-list',
-                    { type: 2 }
-                ).then(res => {
-                    if (Util.isAjaxResOk(res)) {
-                        this.twoList = res.data.data.list
-                    }
-                })
-            },
-            // 获取本页列表
-            getnoticeList3() {
-                axios.post(
-                    '/api/notice/notice-list',
-                    { type: 3 }
-                ).then(res => {
-                    if (Util.isAjaxResOk(res)) {
-                        this.threeList = res.data.data.list
-                    }
-                })
-            },
-            // 获取详情
-            oneDetail(id) {
-                axios.post(
-                    '/api/notice/notice-info',
-                    { notice_id: id }
-                ).then(res => {
-                    if (Util.isAjaxResOk(res)) {
-                        this.drawer = true;
-                        this.detail = res.data.data.notice
-                        this.titleName = res.data.data.notice.type == 1 ? "通知" : (res.data.data.notice.type == 2 ? '公告' : '检查')
-                        this.attachments = res.data.data.notice.attachments
-                        console.log(res)
-                    }
-                })
-            },
-            // 关闭
-            handleClose(done) {
-                done();
-                window.location.reload()
-            },
-            startFlow: function (flowId) {
-                const url = this.url.flowOpen + '?flow=' + flowId + '&uuid=' + this.userUuid;
-                window.open(url, '_blank');
-            },
-            loadFlowsStartedByMe: function () {
-                this.isLoading = true;
-                startedByMe(this.userUuid).then(res => {
-                    if (Util.isAjaxResOk(res)) {
-                        this.flowsStartedByMe = res.data.data.actions;
-                    }
-                    this.isLoading = false;
-                });
-            },
-            loadFlowsWaitingForMe: function () {
-                waitingForMe(this.userUuid).then(res => {
-                    if (Util.isAjaxResOk(res)) {
-                        this.flowsWaitingForMe = res.data.data.actions;
-                    }
-                });
-            },
-            viewApplicationDetail: function (action) {
-                window.location.href = '/pipeline/flow/view-history?action_id=' + action.id;
-            },
-            reloadThisPage: function () {
-                Util.reloadCurrentPage(this);
-            },
-
+        organizansList: [],
+        allOran:false
+      };
+    },
+    created() {
+      const dom = document.getElementById("app-init-data-holder");
+      this.schoolId = dom.dataset.school;
+      this.userUuid = dom.dataset.useruuid;
+      this.url.flowOpen = dom.dataset.flowopen;
+      this.getnoticeList1();
+      this.getnoticeList2();
+      this.getnoticeList3();
+      // this.getOrganizansList()
+    },
+    methods: {
+      checkChange(){
+        console.log(this.$refs.tree.getCheckedNodes());
+        this.selecttags = this.$refs.tree.getCheckedNodes() || []
+      },
+      async loadNode(node, resolve) {
+        if (node.level === 0) {
+          await this.getOrganizansList();
+          return resolve(this.organizansList);
         }
-    });
+        await this.getOrganizansList(node.data.id);
+        resolve(this.organizansList);
+      },
+      handleClose1() {
+        this.releaseDrawer = false;
+      },
+      handleClose2() {
+        this.innerDrawer = false;
+      },
+      deleteTag(tag) {
+        this.selecttags.splice(this.selecttags.indexOf(tag), 1);
+        this.$refs.tree.setCheckedNodes(this.selecttags)
+      },
+      reload() {},
+      async getOrganizansList(parent_id) {
+        await axios
+          .post("/Oa/tissue/getOrganization", {
+            school_id: this.schoolId,
+            parent_id
+          })
+          .then(res => {
+            if (Util.isAjaxResOk(res)) {
+              this.organizansList = res.data.data.organ || [];
+              this.organizansList.forEach(e => (e.status = !e.status));
+            }
+          });
+      },
+      // 最后发布接口
+      release() {},
+      // 获取本页列表
+      getnoticeList1() {
+        axios.post("/api/notice/notice-list", { type: 1 }).then(res => {
+          if (Util.isAjaxResOk(res)) {
+            this.oneList = res.data.data.list;
+          }
+        });
+      },
+      // 获取本页列表
+      getnoticeList2() {
+        axios.post("/api/notice/notice-list", { type: 2 }).then(res => {
+          if (Util.isAjaxResOk(res)) {
+            this.twoList = res.data.data.list;
+          }
+        });
+      },
+      // 获取本页列表
+      getnoticeList3() {
+        axios.post("/api/notice/notice-list", { type: 3 }).then(res => {
+          if (Util.isAjaxResOk(res)) {
+            this.threeList = res.data.data.list;
+          }
+        });
+      },
+      // 获取详情
+      oneDetail(id) {
+        axios.post("/api/notice/notice-info", { notice_id: id }).then(res => {
+          if (Util.isAjaxResOk(res)) {
+            this.drawer = true;
+            this.detail = res.data.data.notice;
+            this.titleName =
+              res.data.data.notice.type == 1
+                ? "通知"
+                : res.data.data.notice.type == 2
+                ? "公告"
+                : "检查";
+            this.attachments = res.data.data.notice.attachments;
+            console.log(res);
+          }
+        });
+      },
+      // 关闭
+      handleClose(done) {
+        done();
+        window.location.reload();
+      },
+      startFlow: function(flowId) {
+        const url =
+          this.url.flowOpen + "?flow=" + flowId + "&uuid=" + this.userUuid;
+        window.open(url, "_blank");
+      },
+      loadFlowsStartedByMe: function() {
+        this.isLoading = true;
+        startedByMe(this.userUuid).then(res => {
+          if (Util.isAjaxResOk(res)) {
+            this.flowsStartedByMe = res.data.data.actions;
+          }
+          this.isLoading = false;
+        });
+      },
+      loadFlowsWaitingForMe: function() {
+        waitingForMe(this.userUuid).then(res => {
+          if (Util.isAjaxResOk(res)) {
+            this.flowsWaitingForMe = res.data.data.actions;
+          }
+        });
+      },
+      viewApplicationDetail: function(action) {
+        window.location.href =
+          "/pipeline/flow/view-history?action_id=" + action.id;
+      },
+      reloadThisPage: function() {
+        Util.reloadCurrentPage(this);
+      }
+    }
+  });
 }
--- resources/views/teacher/ly/oa/notices_center.blade.php
+++ resources/views/teacher/ly/oa/notices_center.blade.php
@@ -99,17 +99,31 @@
           :before-close="handleClose2"
           :visible.sync="innerDrawer"
           custom-class="inner-teacher-drawer"
-          size="40%"
+          size="60%"
         >
           <el-form :model="form">
-            <el-form-item label="搜索" label-width="50px">
+            <!-- <el-form-item label="搜索" label-width="50px">
               <el-input v-model="form.title" autocomplete="off" style="width: 90%;"></el-input>
-            </el-form-item>
+            </el-form-item> -->
             <el-form-item label="部门" label-width="50px">
-              <el-cascader popper-class="pip_flow_mamager_cascader" style="width: 90%;" :props="props" :options="organizansList"  v-model="form.organizations"></el-cascader>
+                <el-tree
+                    ref="tree"
+                    :props="props"
+                    node-key="id"
+                    :load="loadNode"
+                    :check-on-click-node="true"
+                    :check-strictly="true"
+                    @check-change="checkChange"
+                    lazy
+                    show-checkbox>
+                </el-tree>
             </el-form-item>
             <el-form-item label="便捷操作" label-width="80px" style="margin-top: 50px;">
-              <el-button type="primary">所有部门</el-button>
+              <el-switch
+                v-model="allOran"
+                active-text="所有部门"
+                inactive-text="">
+                </el-switch>
             </el-form-item>
             <div style="padding-left: 20px">
               <p>已选部门</p>
@@ -118,13 +132,10 @@
                   v-for="(item, index) in selecttags"
                   :key="index"
                   closable
-                  :type="item.type"
                   @close="deleteTag(item)"
                   style="margin-right: 10px;color: #fff;background-color: #409EFF;position: relative;"
                 >
-                  @{{item.name}}
-                  <svg t="1591152186700" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3250" width="20" height="20"><path d="M544 64C279.04 64 64 279.04 64 544S279.04 1024 544 1024 1024 808.96 1024 544 808.96 64 544 64zM544 960C314.24 960 128 773.76 128 544S314.24 128 544 128C773.76 128 960 314.24 960 544S773.76 960 544 960zM588.16 540.8l168.32-166.4c12.8-12.8 12.8-33.28 0-45.44-12.8-12.8-33.28-12.8-46.08 0L542.08 494.08 376.96 328.32c-12.8-12.8-33.28-12.8-45.44 0-12.8 12.8-12.8 33.28 0 45.44l164.48 165.76-174.72 172.8c-12.8 12.8-12.8 33.28 0 45.44 12.8 12.8 33.28 12.8 46.08 0l174.08-172.16 171.52 172.16c12.8 12.8 33.28 12.8 45.44 0 12.8-12.8 12.8-33.28 0-45.44L588.16 540.8z" p-id="3251" fill="#cdcdcd"></path></svg>
-                </el-tag>
+                  @{{item.name}}</el-tag>
               </div>
             </div>
           </el-form>
