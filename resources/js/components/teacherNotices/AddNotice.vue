0755bc309b4e4faba2a074e21f61312c964ddadb
Cherish666 <791832287@qq.com>
2020-06-07 13:24:31 +0800

    update

--- resources/js/components/teacherNotices/AddNotice.vue
+++ resources/js/components/teacherNotices/AddNotice.vue
@@ -1,56 +1,59 @@
 <template>
 	<div class="drawer_content">
-		<el-form :model="form" label-width="130px" :rules="rules" ref="ruleForm">
+		<el-form :model="form" label-width="55px" :rules="rules" ref="ruleForm">
 			<el-form-item label="标题" prop="title">
 				<el-input v-model="form.title" autocomplete="off"></el-input>
 			</el-form-item>
 			<el-form-item label="内容" prop="textarea">
 				<el-input type="textarea" :rows="4" placeholder="请输入通知内容" v-model="form.textarea"></el-input>
 			</el-form-item>
-			<el-form-item label="选择教师可见范围">
-				<div class="dayu" @click="innerDrawer = true; showOrganizationsSelectorFlag = true;">
-					<template v-if="form.teacherTags == '0'">
-						<el-tag class="Oran-Tag" style>所有部门</el-tag>
-					</template>
-					<template v-else>
-						<el-tag
-							v-for="(item, index) in form.teacherTags"
-							:key="index"
-							class="Oran-Tag"
-							style
-						>{{item.name}}</el-tag>
-					</template>
-					<span class="float-right">></span>
-				</div>
-			</el-form-item>
-			<el-form-item label="选择学生可见范围">
-				<div class="dayu" @click="innerDrawer = true; showOrganizationsSelectorFlag = false;">
-					<template v-if="form.studentTags == '0'">
-						<el-tag class="Oran-Tag" style>所有班级</el-tag>
-					</template>
-					<template v-else>
-						<el-tag
-							v-for="(item, index) in form.studentTags"
-							:key="index"
-							class="Oran-Tag"
-							style
-						>{{item.name}}</el-tag>
-					</template>
-					<span class="float-right">></span>
-				</div>
-			</el-form-item>
-			<el-form-item label="附件" style="font-size: 16px; color: #000;" prop="files">
-				<el-button type="primary" size="small" @click="showFileManager">上传附件</el-button>
-				<div class="fileList">
-					<ul>
-						<li v-for="(file, index) in form.files" :key="file.id">
-							<a :href="file.url">{{file.file_name}}</a>
-							<i @click="deleteFile(index)" class="el-icon-close"></i>
-						</li>
-					</ul>
-				</div>
-			</el-form-item>
 		</el-form>
+		<div class="selectBlock" style="border-top: 1px solid #EAEDF2">
+			<div>选择教师可见范围</div>
+			<div class="dayu" @click="innerDrawer = true; showOrganizationsSelectorFlag = true;">
+				<template v-if="form.teacherTags == '0'">
+					<el-tag class="Oran-Tag" style>所有部门</el-tag>
+				</template>
+				<template v-else>
+					<el-tag
+						v-for="(item, index) in form.teacherTags"
+						:key="index"
+						class="Oran-Tag"
+						style
+					>{{item.name}}</el-tag>
+				</template>
+				<i class="el-icon-arrow-right" style="font-size: 20px;"></i>
+			</div>
+		</div>
+		<div class="selectBlock">
+			<div>选择学生可见范围</div>
+			<div class="dayu" @click="innerDrawer = true; showOrganizationsSelectorFlag = false;">
+				<template v-if="form.studentTags == '0'">
+					<el-tag class="Oran-Tag" style>所有班级</el-tag>
+				</template>
+				<template v-else>
+					<el-tag
+						v-for="(item, index) in form.studentTags"
+						:key="index"
+						class="Oran-Tag"
+						style
+					>{{item.name}}</el-tag>
+				</template>
+				<i class="el-icon-arrow-right" style="font-size: 20px;"></i>
+			</div>
+		</div>
+		<div class="fujian">
+			<p class="text1">附件  <span class="text2"> (图片格式)</span></p>
+			<div class="fileList">
+				<ul>
+					<li v-for="(file, index) in form.files" :key="file.id">
+						<a :href="file.url">{{file.file_name}}</a>
+						<i @click="deleteFile(index)" class="el-icon-close"></i>
+					</li>
+				</ul>
+			</div>
+			<el-button type="primary" size="small" @click="showFileManager" style="margin-top: 10px">上传附件</el-button>
+		</div>
 		<div class="btn-tools">
 			<el-button type="primary" @click="release" style="padding: 12px 40px;">发布</el-button>
 		</div>
@@ -200,19 +203,36 @@ export default {
 <style lang="scss" scoped>
 .btn-tools {
 	text-align: center;
+	margin-top: 50px;
+}
+.selectBlock {
+	display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #EAEDF2;
+	padding: 15px 0;
 }
 .dayu {
 	color: #ccc;
-	font-size: 32px;
-	border-bottom: 1px solid #ccc;
+	// font-size: 32px;
+	// border-bottom: 1px solid #ccc;
 	cursor: pointer;
-	text-align: left;
-	padding-bottom: 10px;
+	// text-align: left;
+	// padding-bottom: 10px;
 }
 .dayu:after {
 	content: "";
 	display: table;
 	clear: both;
+}
+.fujian {
+	padding: 15px 0;
+	.text1 {
+		margin-bottom: 10px;
+	}
+	.text2 {
+		color: #90939a;
+    	font-size: 13px;
+	}
 }
 </style>
 
--- resources/js/components/teacherNotices/VisibleRangeForS.vue
+++ resources/js/components/teacherNotices/VisibleRangeForS.vue
@@ -1,42 +1,33 @@
 <template>
-  <el-form label-width="90px">
-    <el-form-item>
-      <el-tag
-        @click="checkAll"
-        class="organ-item"
-        effect="plain">
-        所有班级
-      </el-tag>
-      <el-tag
-        v-if="isAdviser"
-        class="organ-item"
-        @click="loadYear"
-        effect="plain">
-        我的班级
-      </el-tag>
-    </el-form-item>
-    <el-form-item label="年级：" v-if="!all">
-       <el-tag
-        v-for="item in years"
-        class="organ-item"
-        :key="item.year"
-        @click="loadGrade(item)"
-        effect="plain">
-        {{item.text}}
-      </el-tag>
-    </el-form-item>
-    <el-form-item label="班级：" v-if="!all">
-       <el-tag
-        v-for="item in grades"
-        :key="item.grade_id"
-        class="organ-item"
-        @click="checkGrade(item)"
-        effect="plain">
-        {{item.name}}
-      </el-tag>
-    </el-form-item>
-    <el-form-item label="已选班级：" >
-      <template v-if="!all">
+  <el-form>
+    <el-form-item label="搜索" label-width="60px" >
+			<el-autocomplete
+				v-model="form.title"
+				:fetch-suggestions="querySearchAsync"
+				placeholder="请输入内容"
+				style="width:100%;"
+				@select="handleSelect"
+				>
+				<template slot-scope="{ item }">
+					<div class="name">{{ item.name }}</div>
+				</template>
+				</el-autocomplete>
+		</el-form-item>
+		<el-form-item label="班级" label-width="60px">
+      <el-checkbox-group v-model="selectTags">
+        <el-cascader style="width:100%;" popper-class="tags_cascader" ref="tags" :props="props" :show-all-levels="false" v-model="form.tags">
+          <template slot-scope="{ node, data }">
+            <span v-if="node.level == 1">{{ data.text }}</span>
+            <el-checkbox v-else :label="data.name" :value="data.grade_id"></el-checkbox>
+          </template>
+        </el-cascader>
+      </el-checkbox-group>
+		</el-form-item>
+  	<el-form-item label="便捷操作" label-width="90px" style="margin-top: 50px;">
+			<el-switch v-model="form.allOran" active-text="所有班级" inactive-text></el-switch>
+		</el-form-item>
+    <el-form-item label="已选班级：" v-if="!form.allOran" >
+      <template>
         <el-tag
           v-for="(item, index) in selectTags"
           :key="index"
@@ -45,14 +36,6 @@
           @close="deleteTag(item)"
           style="margin-right: 10px;color: #fff;background-color: #409EFF;position: relative;"
         >{{item.name}}</el-tag>
-      </template>
-      <template v-else>
-        <el-tag
-          class="Oran-Tag"
-          closable
-          @close="deleteAllTag"
-          style="margin-right: 10px;color: #fff;background-color: #409EFF;position: relative;"
-        >所有班级</el-tag>
       </template>
 		</el-form-item>
     <div class="btn-tools">
@@ -71,12 +54,27 @@ export default {
       selectTags: [],
       isAdviser:false,
       years:[],
-      grades:[]
+      grades:[],
+      form: {
+				allOran: false,
+				title:'',
+				tags:[]
+      },
+      props: {
+        lazy: true,
+        value:'year',
+				lazyLoad :async (node, resolve) => {
+          console.log(node)
+          if(node.level == 0){
+            await this.getYears();
+            resolve(this.years)
+            return;
+          }
+          await this.getGrade(node.data)
+					resolve(this.grades)
+				}
+			}
 		};
-	},
-	mounted() {
-    this.LoadisAdviser()
-    this.getYears()
 	},
 	methods: {
      LoadisAdviser(){
@@ -87,9 +85,7 @@ export default {
 					}
 				});
     },
-    checkAll(){
-      this.all = true
-    },
+   
     deleteAllTag(){
       this.all = false;
       this.selectTags = [];
@@ -99,42 +95,45 @@ export default {
     },
     confrim(){
       console.log('confrim')
-      this.$emit('confrim',this.visibleform.allOran ? 'all' : this.selectTags)
+      this.$emit('confrim',this.form.allOran ? '0' : this.selectTags)
     },
-    getYears(){
-        axios.get(`/api/school/load-config-year?school_id=${this.schoolId}`).then(res => {
+    async getYears(){
+       await axios.get(`/api/school/load-config-year?school_id=${this.schoolId}`).then(res => {
 					if (Util.isAjaxResOk(res)) {
             console.log(res)
             this.years = res.data.data;
 					}
 				});
     },
-    getGrade(item){
-        axios.get(`/api/notice/grade-list?year=${item.year}`).then(res => {
+    async getGrade(item,keyword){
+        await axios.get(`/api/notice/grade-list?year=${item.year || 0}&keyword=${keyword}`).then(res => {
 					if (Util.isAjaxResOk(res)) {
             console.log(res)
             this.grades = res.data.data;
 					}
-				});
-    },
-    loadYear(){
-      this.all = false;
-      this.selectTags = [];
-    },
-    loadGrade(item){
-      this.getGrade(item)
-    },
-    checkGrade(item){
-     	let sign = this.selectTags.findIndex(e => e.grade_id == item.grade_id);
-      if (sign != -1) {
-        this.selectTags.splice(sign, 1);
-      } else {
-        this.selectTags.push(item);
-      }
+        });
+        return this.grades;
     },
     confrim(){
-      this.$emit('confrim',this.all ? '0' : this.selectTags)
-    }
+      this.$emit('confrim',this.form.allOran ? '0' : this.selectTags)
+    },
+    handleSelect(item){
+			console.log(item)
+			if(!this.selectTags.find(e => e.grade_id == item.grade_id)){
+				this.selectTags.push(item);
+				this.form.tags.push(item.id)
+			}
+
+		},
+		deleteTag(tag) {
+			this.selectTags.splice(this.selectTags.indexOf(tag), 1);
+			console.log('deleteTag',tag)
+			this.form.tags = this.form.tags.filter(arr => !arr.includes(tag.id))
+    },
+    async querySearchAsync(queryString, cb){
+			let items = await  this.getGrade({},queryString)
+			cb(items || [])
+		},
 	}
 };
 </script>
